5001a2811f41e47c7c1bcb7105fa4c35b38db960
Mario Pareja <pareja.mario@gmail.com>
2018-03-13 08:34:29 -0400

    add queuing-api-decorator
    Planning on using it for tree walking without doing an overwhelming number of either HTTP requests or File System queries

---
+++ test/queuing-api-decorator-test.js
@@ -0,0 +1,59 @@
+const createQueuingApi = require('../lib/queuing-api-decorator.js')
+const promisify = require('util').promisify
+const sinon = require('sinon')
+
+const delay = promisify(setTimeout)
+
+describe('queuing-api-decorator', () => {
+  let stubApi
+  beforeEach(() => {
+    stubApi = {
+      getGroupCollections: sinon.spy(async () => delay(10)),
+      getActivityGroup: sinon.spy(async () => delay(10)),
+      getActivitiesInGroup: sinon.spy(async () => delay(10))
+    }
+  })
+
+  it('supports limitting concurrency to 1 at a time', async () => {
+    const api = createQueuingApi(stubApi, 1)
+    api.getGroupCollections()
+    api.getActivityGroup()
+    api.getActivitiesInGroup()
+
+    await delay(5) // t = 5
+
+    sinon.assert.calledOnce(stubApi.getGroupCollections)
+    sinon.assert.notCalled(stubApi.getActivityGroup)
+    sinon.assert.notCalled(stubApi.getActivitiesInGroup)
+
+    await delay(6) // t = 11
+
+    sinon.assert.calledOnce(stubApi.getGroupCollections)
+    sinon.assert.calledOnce(stubApi.getActivityGroup)
+    sinon.assert.notCalled(stubApi.getActivitiesInGroup)
+
+    await delay(11) // t = 22
+    sinon.assert.calledOnce(stubApi.getGroupCollections)
+    sinon.assert.calledOnce(stubApi.getActivityGroup)
+    sinon.assert.calledOnce(stubApi.getActivitiesInGroup)
+  })
+
+  it('supports limitting concurrency to 2 at a time', async () => {
+    const api = createQueuingApi(stubApi, 2)
+    api.getGroupCollections()
+    api.getActivityGroup()
+    api.getActivitiesInGroup()
+
+    await delay(5) // t = 5
+
+    sinon.assert.calledOnce(stubApi.getGroupCollections)
+    sinon.assert.calledOnce(stubApi.getActivityGroup)
+    sinon.assert.notCalled(stubApi.getActivitiesInGroup)
+
+    await delay(6) // t = 11
+
+    sinon.assert.calledOnce(stubApi.getGroupCollections)
+    sinon.assert.calledOnce(stubApi.getActivityGroup)
+    sinon.assert.calledOnce(stubApi.getActivitiesInGroup)
+  })
+})
